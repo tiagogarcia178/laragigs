5f1c07ba2de85b5d0af881bfc4e65b077183ec67
tiagogarcia178 <tiagobgarcia@icloud.com>
2022-12-08 17:24:22 +0000

    added clockwork, the listing form view, and the code to validate the form and create a new listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
-    //get and show all listings
+    //Get and show all listings
     public function index()
     {
         //dd(request('tag')); //request params sent to the URL
@@ -17,11 +18,36 @@ class ListingController extends Controller
                 ->get(),
         ]);
     }
-    //show single listing
+    //Show single listing
     public function show(Listing $listing)
     {
         return view('listings.show', [
             'listing' => $listing,
         ]);
+    }
+
+    //Show create form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    //Store Listing Data
+    public function store(Request $request)
+    {
+        /* dd($request->all()); */
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        Listing::create($formFields);
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::get('/', [ListingController::class, 'index']);
 
 //Single Listing
 Route::get('/listing/{listing}', [ListingController::class, 'show']);
+
+//Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);

048fd64072cc46817f23affd0f10ccd99ff76433
tiagogarcia178 <tiagobgarcia@icloud.com>
2022-12-08 13:03:44 +0000

    added the tags component

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -13,6 +13,8 @@
                 <h3 class="text-2xl mb-2">{{ $listing->title }}</h3>
                 <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
 
+                <x-listing-tags :tagsCsv="$listing->tags" />
+
                 <div class="text-lg my-4">
                     <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
                 </div>

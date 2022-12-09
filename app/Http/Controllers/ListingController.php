8480caa996de13530c78ee71d57e5f3587badba0
tiagogarcia178 <tiagobgarcia@icloud.com>
2022-12-09 16:49:52 +0000

    file upload and show image

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -46,8 +46,12 @@ class ListingController extends Controller
             'description' => 'required',
         ]);
 
+        if ($request->hasFile('logo')) {
+            $formFields['logo'] = $request
+                ->file('logo')
+                ->store('logos', 'public');
+        }
         Listing::create($formFields);
-
         return redirect('/')->with('message', 'Listing Created successfully!');
     }
 }
--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -7,12 +7,12 @@
             <p class="mb-4">Post a gig to find a developer</p>
         </header>
 
-        <form method="POST" action="/listings">
+        <form method="POST" action="/listings" enctype="multipart/form-data">
             @csrf
             <div class="mb-6">
                 <label for="company" class="inline-block text-lg mb-2">Company Name</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full"
-                    name="company"value={{ old('company') }} />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
+                    value="{{ old('company') }}" />
 
                 @error('company')
                     <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
@@ -22,7 +22,7 @@
             <div class="mb-6">
                 <label for="title" class="inline-block text-lg mb-2">Job Title</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
-                    placeholder="Example: Senior Laravel Developer" value={{ old('title') }} />
+                    placeholder="Example: Senior Laravel Developer" value="{{ old('title') }}" />
                 @error('title')
                     <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
                 @enderror
@@ -31,7 +31,7 @@
             <div class="mb-6">
                 <label for="location" class="inline-block text-lg mb-2">Job Location</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-                    placeholder="Example: Remote, Boston MA, etc" value={{ old('location') }} />
+                    placeholder="Example: Remote, Boston MA, etc" value="{{ old('location') }}" />
                 @error('location')
                     <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
                 @enderror
@@ -40,7 +40,7 @@
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Contact Email</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email"
-                    placeholder="Example: jhon@gmail.com" value={{ old('email') }} />
+                    placeholder="Example: jhon@gmail.com" value="{{ old('email') }}" />
                 @error('email')
                     <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
                 @enderror
@@ -51,7 +51,7 @@
                     Website/Application URL
                 </label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="website"
-                    value={{ old('website') }} />
+                    value="{{ old('website') }}" />
                 @error('website')
                     <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
                 @enderror
@@ -62,18 +62,21 @@
                     Tags (Comma Separated)
                 </label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
-                    placeholder="Example: Laravel, Backend, Postgres, etc" value={{ old('tags') }} />
+                    placeholder="Example: Laravel, Backend, Postgres, etc" value="{{ old('tags') }}" />
                 @error('tags')
                     <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
                 @enderror
             </div>
 
-            {{-- <div class="mb-6">
+            <div class="mb-6">
                 <label for="logo" class="inline-block text-lg mb-2">
                     Company Logo
                 </label>
                 <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo" />
-            </div> --}}
+                @error('logo')
+                    <p class="text-red-500 text-xs-mt-1">{{ $message }}</p>
+                @enderror
+            </div>
 
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">
